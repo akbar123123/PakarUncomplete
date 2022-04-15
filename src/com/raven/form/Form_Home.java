5b342981be9f81675ee76580a8b51b370a915940
Akbar Ramadhani Firdaus <54605434+akbar123123@users.noreply.github.com>
2022-04-15 21:36:55 +0700

    Add files via upload

---
+++ src/com/raven/form/Form_Home.java
@@ -0,0 +1,144 @@
+package com.raven.form;
+
+import com.raven.model.Model_Card;
+import com.raven.model.StatusType;
+import com.raven.swing.ScrollBar;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class Form_Home extends javax.swing.JPanel {
+
+    public Form_Home() {
+        initComponents();
+        card1.setData(new Model_Card(new ImageIcon(getClass().getResource("/com/raven/icon/guru.png")), "Guru", "00"));
+        card2.setData(new Model_Card(new ImageIcon(getClass().getResource("/com/raven/icon/karyawan.png")), "Karyawan", "00"));
+        card3.setData(new Model_Card(new ImageIcon(getClass().getResource("/com/raven/icon/satpam.png")), "Satpam", "00"));
+        //  add row table
+        spTable.setVerticalScrollBar(new ScrollBar());
+        spTable.getVerticalScrollBar().setBackground(Color.WHITE);
+        spTable.getViewport().setBackground(Color.WHITE);
+        JPanel p = new JPanel();
+        p.setBackground(Color.WHITE);
+        spTable.setCorner(JScrollPane.UPPER_RIGHT_CORNER, p);
+        table.addRow(new Object[]{"Muhammad Adi Saputro", "Karyawan", "07:54:00", StatusType.REJECT});
+        table.addRow(new Object[]{"Muhammad Maulana Malik Ibrahim", "Guru", "06:54:00", StatusType.APPROVED});
+        table.addRow(new Object[]{"Yunanta Dwi Kristanto", "Karyawan", "07:54:00", StatusType.REJECT});
+        table.addRow(new Object[]{"Nabila Zhafarina", "Satpam", "06:54:00", StatusType.APPROVED});
+        table.addRow(new Object[]{"Akbar Ramadhani Firdaus", "Karyawan", "07:54:00", StatusType.REJECT});
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panel = new javax.swing.JLayeredPane();
+        card1 = new com.raven.component.Card();
+        card2 = new com.raven.component.Card();
+        card3 = new com.raven.component.Card();
+        panelBorder1 = new com.raven.swing.PanelBorder();
+        jLabel1 = new javax.swing.JLabel();
+        spTable = new javax.swing.JScrollPane();
+        table = new com.raven.swing.Table();
+
+        setBackground(new java.awt.Color(242, 242, 242));
+
+        panel.setLayout(new java.awt.GridLayout(1, 0, 10, 0));
+
+        card1.setColor1(new java.awt.Color(157, 246, 255));
+        card1.setColor2(new java.awt.Color(66, 194, 255));
+        panel.add(card1);
+
+        card2.setColor1(new java.awt.Color(157, 246, 255));
+        card2.setColor2(new java.awt.Color(66, 194, 255));
+        panel.add(card2);
+
+        card3.setColor1(new java.awt.Color(157, 246, 255));
+        card3.setColor2(new java.awt.Color(66, 194, 255));
+        panel.add(card3);
+
+        panelBorder1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(127, 127, 127));
+        jLabel1.setText("Baru Presensi");
+
+        spTable.setBorder(null);
+
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nama", "Pekerjaan", "Waktu Absen", "Status"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        table.setFont(new java.awt.Font("Montserrat", 1, 14)); // NOI18N
+        spTable.setViewportView(table);
+
+        javax.swing.GroupLayout panelBorder1Layout = new javax.swing.GroupLayout(panelBorder1);
+        panelBorder1.setLayout(panelBorder1Layout);
+        panelBorder1Layout.setHorizontalGroup(
+            panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBorder1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelBorder1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(spTable))
+                .addContainerGap())
+        );
+        panelBorder1Layout.setVerticalGroup(
+            panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBorder1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(spTable, javax.swing.GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE)
+                .addGap(20, 20, 20))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, 875, Short.MAX_VALUE))
+                .addGap(20, 20, 20))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(20, 20, 20))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.raven.component.Card card1;
+    private com.raven.component.Card card2;
+    private com.raven.component.Card card3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLayeredPane panel;
+    private com.raven.swing.PanelBorder panelBorder1;
+    private javax.swing.JScrollPane spTable;
+    private com.raven.swing.Table table;
+    // End of variables declaration//GEN-END:variables
+}
